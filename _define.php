f3ae6ec057c768bc80a69f336721790edde7c39a
franck <carnet.franck.paul@gmail.com>
2015-08-29 12:50:34 +0200

    Next release will be 0.3

--- _define.php
+++ _define.php
@@ -16,7 +16,7 @@ $this->registerModule(
 	/* Name */			"Rosetta",
 	/* Description*/		"Manage post/page translations",
 	/* Author */			"Franck Paul",
-	/* Version */			'0.2',
+	/* Version */			'0.3',
 	array(
 		/* Permissions */	'permissions' =>	'usage,contentadmin',
 		/* Priority */		'priority' =>		1001,	// Must be higher then pages plugin
