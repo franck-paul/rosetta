7344c09ccd418b32aa61fae2e401395d45f0d5a1
franck <carnet.franck.paul@gmail.com>
2016-03-21 11:42:10 +0100

    Local getAcceptLanguages() function not more needed as CB has been fixed with 2.9

--- inc/rosetta.behaviors.php
+++ inc/rosetta.behaviors.php
@@ -369,7 +369,7 @@ class rosettaPublicBehaviors
 					// Only operate when not counting (aka getPosts() called with $count_only = true)
 					rosettaPublicBehaviors::$state = ROSETTA_SWITCH;
 					// replace translated posts if any may be using core->getPosts()
-					$langs = self::getAcceptLanguages();
+					$langs = http::getAcceptLanguages();
 					if (count($langs)) {
 						$ids = array();
 						$nbx = 0;
@@ -519,48 +519,6 @@ class rosettaPublicBehaviors
 		return false;
 	}
 
-	/**
-	* Client languages
-	*
-	* Returns an array of accepted langages ordered by priority.
-	* can be a two letters language code or a xx-xx variant.
-	*
-	* @return array
-	*/
-	private static function getAcceptLanguages()
-	{
-		if (version_compare(DC_VERSION,'2.9','<'))
-		{
-			$langs = array();
-			if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
-
-				// break up string into pieces (languages and q factors)
-				preg_match_all('/([a-z]{1,8}(-[a-z]{1,8})?)\s*(;\s*q\s*=\s*(1|0\.[0-9]+))?/i',
-					$_SERVER['HTTP_ACCEPT_LANGUAGE'],$lang_parse);
-
-				if (count($lang_parse[1])) {
-					// create a list like "en" => 0.8
-					$langs = array_combine($lang_parse[1],$lang_parse[4]);
-
-					// set default to 1 for any without q factor
-					foreach ($langs as $lang => $val) {
-						if ($val === '') {
-							$langs[$lang] = 1;
-						}
-					}
-
-					// sort list based on value
-					arsort($langs,SORT_NUMERIC);
-					$langs = array_map('strtolower',array_keys($langs));
-				}
-			}
-			return $langs;
-		} else {
-			// Fixed on CB 0.9 included in Dotclear 2.9+
-			return http::getAcceptLanguages();
-		}
-	}
-
 	public static function urlHandlerGetArgsDocument($handler)
 	{
 		global $core;
@@ -583,7 +541,7 @@ class rosettaPublicBehaviors
 			$handler->getArgs($_SERVER['URL_REQUEST_PART'],$urlType,$urlPart);
 			if (in_array($urlType,array('post','pages'))) {
 				// It is a post or page: Try to find a translation according to the browser settings
-				$langs = self::getAcceptLanguages();
+				$langs = http::getAcceptLanguages();
 			}
 		}
 
